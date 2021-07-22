000c3cb6e281edda2d6be0fa8114dd3211790cc3
Charl Meyers <charl.meyers@standardbank.co.za>
2021-07-22 17:40:05 +0200

    Populate

--- CompanyCup/enums/Resource.cs
+++ CompanyCup/enums/Resource.cs
@@ -2,6 +2,15 @@
 {
     public enum Resource
     {
-        
+        Adamantium = 1,
+        Madamantium = 2,
+        Sadamantium = 3,
+        Gladamantium = 4,
+        Radamantium = 5,
+        Badamantium = 6,
+        Chocolate = 7,
+        Antmanium = 8,
+        Vladamantium = 9,
+        Vibranium = 10
     }
 }
